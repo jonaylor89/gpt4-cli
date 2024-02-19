e8f8ec2bbf5072779e0c5c41eb15d61d316e3e9e
Johannes Naylor <jonaylor89@gmail.com>
2024-02-19 02:36:06 -0500

    add parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use clap::ValueEnum;
 use reqwest::header::CONTENT_TYPE;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 #[derive(Deserialize, Debug)]
@@ -16,41 +17,70 @@ struct OpenAiResponse {
     choices: Vec<Choice>,
 }
 
-pub async fn llm(prompt: &str, token: &str) -> Result<String, reqwest::Error> {
-    let url = "https://api.openai.com/v1/chat/completions";
-    let model_name = "gpt-4-1106-preview";
+#[derive(Debug, ValueEnum, Serialize, Default, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub enum LlmOutputFormat {
+    Plaintext,
 
-    let data = json!({
-        "model": model_name,
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant, keep your answers in markdown"
-            },
-            {
-                "role": "user",
-                "content": prompt
+    #[default]
+    Markdown,
+
+    Html,
+}
+
+#[derive(Debug)]
+pub struct Llm<'llm> {
+    pub token: &'llm str,
+
+    pub format: &'llm LlmOutputFormat,
+
+    pub model_name: &'llm str,
+}
+
+impl Llm<'_> {
+    pub async fn call(&self, prompt: &str) -> Result<String, reqwest::Error> {
+        let url = "https://api.openai.com/v1/chat/completions";
+
+        let system_content = match self.format {
+            LlmOutputFormat::Plaintext => "You are a helpful assistant",
+            LlmOutputFormat::Markdown => {
+                "You are a helpful assistant, keep your answers in markdown"
             }
-        ]
-    });
+            LlmOutputFormat::Html => "You are a helpful assistant, keep your answers as html",
+        };
 
-    let client = reqwest::Client::new();
-    let body = client
-        .post(url)
-        .header(CONTENT_TYPE, "application/json")
-        .bearer_auth(token)
-        .body(data.to_string())
-        .send()
-        .await?
-        .json::<OpenAiResponse>()
-        .await?;
+        let data = json!({
+            "model": self.model_name,
+            "messages": [
+                {
+                    "role": "system",
+                    "content": system_content,
+                },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+        });
 
-    if cfg!(debug_assertions) {
-        println!("Prompt: {}", prompt);
-        dbg!("Response: {:?}", &body);
+        let client = reqwest::Client::new();
+        let body = client
+            .post(url)
+            .header(CONTENT_TYPE, "application/json")
+            .bearer_auth(self.token)
+            .body(data.to_string())
+            .send()
+            .await?
+            .json::<OpenAiResponse>()
+            .await?;
+
+        if cfg!(debug_assertions) {
+            println!("Prompt: {}", prompt);
+            dbg!("Response: {:?}", &body);
+        }
+
+        let response = body.choices[0].message.content.clone();
+
+        Ok(response)
     }
-
-    let response = body.choices[0].message.content.clone();
-
-    Ok(response)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
-use std::env;
+extern crate clap;
+
+use anyhow::{Error, Result};
+use clap::{arg, command, value_parser};
 use config::Config;
-use gpt4::llm;
-use anyhow::Result;
+use gpt4::{Llm, LlmOutputFormat};
 use serde::Deserialize;
+use std::env;
+use tokio::main;
 
 #[derive(Debug, Deserialize)]
 struct ConfigFile {
     openai_api_key: String,
 }
 
-#[tokio::main]
+#[main]
 async fn main() -> Result<()> {
+    let matches = command!()
+        .arg(
+            arg!(
+                -f --format <FORMAT> "Sets the output format"
+            )
+            .required(false)
+            .value_parser(value_parser!(LlmOutputFormat)),
+        )
+        .arg(
+            arg!(
+                [PROMPT]... "The prompt the for llm"
+            )
+            .required(true),
+        )
+        .get_matches();
+
+    let prompt = matches
+        .get_one::<String>("PROMPT")
+        .ok_or(Error::msg("prompt expected"))?;
+
+    let format = matches
+        .get_one::<LlmOutputFormat>("format")
+        .unwrap_or(&LlmOutputFormat::Markdown);
+
     let config_location = dirs::home_dir()
         .unwrap()
         .join(".config")
         .join("gpt4-cli")
         .join("config.toml");
+
     let settings = Config::builder()
         .add_source(config::File::from(config_location))
         .build()
@@ -27,14 +56,13 @@ async fn main() -> Result<()> {
 
     let openai_api_key = cli_config.openai_api_key;
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: gpt4 <prompt>");
-        return Ok(());
-    }
-
-    let prompt = &args[1..].join(" ");
-    let output = llm(prompt, &openai_api_key).await?;
+    let model_name = "gpt-4-1106-preview";
+    let llm = Llm {
+        token: &openai_api_key,
+        format,
+        model_name,
+    };
+    let output = llm.call(prompt).await?;
 
     termimad::print_text(&output);
 
